44780cf0876195ac3c8a0423c913c1f4e5a67a1e
Daniel Cronqvist <dancro99@gmail.com>
2021-06-19 15:55:25 +0200

    added documentation for get accounts and fixed bug when specifying no accounts

--- api_v1/namespaces/accounts.py
+++ api_v1/namespaces/accounts.py
@@ -11,15 +11,30 @@ api = Namespace("accounts", path="/economy/accounts", description="Information a
 get_requests = reqparse.RequestParser()
 get_requests.add_argument("number", type=int, location="args", required=False, action="append")
 
+get_doc = """
+### Retrieval of economy accounts
+
+Using this endpoint, you can retrieve information about the different accounts tied to your user. 
+
+Specifying no numbers at all will retrieve all accounts available, while specifying any number of account numbers will retrieve those that match any accounts.
+
+#### Privileges
+- economy
+- accounts
+"""
+
 @api.route("")
 class AccountsResource(Resource):
 
+    @api.doc(description=get_doc)
     @api.expect(get_requests, validate=True)
+    @privilege_required("accounts")
+    @privilege_required("economy")
     def get(self):
         args = get_requests.parse_args()
         succ, username = get_username_from_token(request.headers.get("Authorization"))
 
-        if len(args) == 0:
+        if not args["number"]:
             accs = coll_accounts.find({"user": username }, {"_id": 0}, sort=[("number", 1)])
             return list(accs), 200
         else:
